0f18c4ff34f05a4ff144ca576da09eb4226a9987
saniok017 <saniok017@gmail.com>
2019-05-03 00:55:51 +0300

    fix: - reset variables

--- server/passport/telegramStrategy.js
+++ server/passport/telegramStrategy.js
@@ -1,9 +1,7 @@
-const {
-  Strategy
-} = require('passport-telegram');
+const { Strategy } = require('passport-telegram');
 const config = require('../config/config');
 const fetch = require('isomorphic-unfetch');
-
+console.log(config.telegramAuth);
 const telegramStrategy = new Strategy(
   config.telegramAuth,
   (accessToken, refreshToken, profile, done) => {
