77090e86c18b9e18658c8da9b65f2faf80bcd6ab
Dmitry <jimmaggot@mail.ru>
2019-05-03 14:26:23 +0300

    fix: update user fields

--- server/models/user.js
+++ server/models/user.js
@@ -21,6 +21,30 @@ const UserSchema = new Schema({
   username: {
     type: String,
     required: true
+  },
+  banned: {
+    type: Boolean,
+    required: true
+  },
+  events: {
+    type: Array,
+    required: true
+  },
+  company: {
+    type: String,
+    required: true
+  },
+  department: {
+    type: String,
+    required: true
+  },
+  birthday: {
+    type: Date,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true
   }
 });
 
