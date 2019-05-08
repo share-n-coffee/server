01d3587cea97d907949d5310e82f6bc81999d037
Dmitry <jimmaggot@mail.ru>
2019-05-08 18:52:51 +0300

    feat: move mongoose.connect() to connectDatabase file

--- server/database/dbController.js
+++ server/database/dbController.js
@@ -1,60 +1,24 @@
 const mongoose = require('mongoose');
-const config = require('../config/config.js');
 const Users = require('./models/user.js');
 const Events = require('./models/event.js');
 const Departments = require('./models/department.js');
 
 function DBController() {
-  const successMessage = () => {
-    console.log('Database is connected');
-  };
-
-  const errorMessage = error => {
-    console.log(`Can not connect to the database ${error}`);
-  };
-
-  const createRequest = query => {
-    return new Promise((resolve, reject) => {
-      mongoose
-        .connect(
-          config.database,
-          {
-            useNewUrlParser: true,
-            useCreateIndex: true
-          }
-        )
-        .then(() => {
-          successMessage();
-          resolve(query);
-        })
-        .catch(err => {
-          reject(new Error(`Can not connect to the database ${err}`));
-          errorMessage();
-        });
-    });
-  };
-
-  // use example:
-  //
-  // router.get('/users/:user_telegram_id', (req, res) => {
-  //   DBController.getUserByTelegramId(req.params.user_telegram_id)
-  //     .then(user => res.status(200).send(user))
-  //     .catch(error => res.status(422).send(error));
-  // });
-
   const getUserByTelegramId = id => {
     const query = Users.findOne({ id_telegram: id }).exec();
-    return createRequest(query);
+    return new Promise(query);
   };
 
   const getEventById = eventId => {
-    const query = Events.findOne({ _id: mongoose.Types.ObjectId(eventId) }).exec();
-    return createRequest(query);
+    const query = Events.findOne({
+      _id: mongoose.Types.ObjectId(eventId)
+    }).exec();
+    return new Promise(query);
   };
 
   const getAllEvents = () => {
     const query = Events.find({}).exec();
-    return createRequest(query);
+    return new Promise(query);
   };
 
   return { getUserByTelegramId, getEventById, getAllEvents };
