8be70304804f3c152cf07bbe013e95383fb30995
DasneiN <sank.mail@gmail.com>
2019-05-09 15:32:52 +0300

    rewrite all routes using DBController

--- server/api/api.js
+++ server/api/api.js
@@ -1,53 +1,55 @@
+const express = require('express');
+const mongoose = require('mongoose');
 const config = require('../config/config');
-const express = require("express");
-const router = express.Router();
-
 const DBController = require('./../database/dbController');
+
+const router = express.Router();
 
 router.get('/users/:user_telegram_id', (req, res) => {
   DBController.getUserByTelegramId(req.params.user_telegram_id)
-    .then(user => res.status(200).send(user))
+    .then(user =>
+      res
+        .status(200)
+        .set('Content-Type', 'application/json')
+        .send(user)
+    )
     .catch(error => res.status(422).send(error));
 });
 
 router.get('/events/:event_id', (req, res) => {
   DBController.getEventById(req.params.event_id)
-    .then(event => res.status(200).send(event))
+    .then(event =>
+      res
+        .status(200)
+        .set('Content-Type', 'application/json')
+        .send(event)
+    )
     .catch(error => res.status(422).send(error));
 });
 
 router.get('/events/', (req, res) => {
   DBController.getAllEvents()
-    .then(events => res.status(200).send(events))
+    .then(events =>
+      res
+        .status(200)
+        .set('Content-Type', 'application/json')
+        .send(events)
+    )
     .catch(error => res.status(422).send(error));
 });
 
-router.post('/users/:user_telegram_id/:department_id', (req, res) => {
-  mongoose
-    .connect(config.database, {
-      useNewUrlParser: true,
-      useCreateIndex: true
-    })
-    .then(() => {
-      console.log('Database is connected');
-      console.log(req.params);
-
-      demo_users.findOneAndUpdate(
-        { 'id_telegram': req.params.user_telegram_id },
-        {
-          $set: { 'department': new mongoose.Types.ObjectId(req.params.department_id) }
-        },
-        (err, data) => {
-          console.log('Department updated!');
-          console.log(data);
-          res.status(200).send(data);
-        }
-      );
-    })
-    .catch(err => {
-      res.status(422).send({ 'Error': `Can not connect to the database ${err}` });
-      console.log(`Can not connect to the database ${err}`);
-    });
+router.put('/users/:user_telegram_id/:department_id', (req, res) => {
+  DBController.setUserDepartment(
+    req.params.user_telegram_id,
+    req.params.department_id
+  )
+    .then(user =>
+      res
+        .status(200)
+        .set('Content-Type', 'application/json')
+        .send(user)
+    )
+    .catch(error => res.status(422).send(error));
 });
 
 module.exports = router;
