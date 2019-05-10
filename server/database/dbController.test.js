2e3760dad03c23fc9f6628dc8bde17a82e4e18ab
saniok017 <saniok017@gmail.com>
2019-05-10 23:53:04 +0300

    feat: - add tests

--- server/database/dbController.test.js
+++ server/database/dbController.test.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
-const connectDatabase = require('./connectDatabase.js');
+const connectDatabase = require('../lib/connectDatabase');
 const demoUsers = require('./models/demo/user.js');
 const demoEvents = require('./models/demo/event.js');
 const demoDepartments = require('./models/demo/department.js');
 const controller = require('./dbController.js');
 
-controller.bind(null, demoUsers, demoEvents, demoDepartments);
+// controller.bind(null, demoUsers, demoEvents, demoDepartments);
 
 connectDatabase();
 
-console.log(demoUsers);
+describe('dbcontroller tests', () => {
+  it('config test', () => {
+    expect(controller).toBeTruthy();
+  });
+});
