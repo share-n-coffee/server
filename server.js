5f0ab2bdf62806d2b45d5068fa9818b5ab9864a1
saniok017 <saniok017@gmail.com>
2019-05-10 17:43:31 +0300

    refactor: - change db connection

--- server.js
+++ server.js
@@ -1,33 +1,20 @@
 require('dotenv').config();
 
 const express = require('express');
-const mongoose = require('mongoose');
 const middlewares = require('./server/middleware/middleware');
 const routesHandle = require('./server/routes/routesHandle');
-const config = require('./server/config/config');
+const connectDatabase = require('./server/lib/connectDatabase');
 
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 
 const app = express();
 
-mongoose
-  .connect(
-    config.database,
-    {
-      useNewUrlParser: true,
-      useCreateIndex: true
-    }
-  )
-  .then(() => {
-    console.log('Database is connected');
-  })
-  .catch(err => {
-    console.log(`Can not connect to the database ${err}`);
-  });
+connectDatabase();
 
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/server/views`);
+
 middlewares(app);
 routesHandle(app);
 app.use('/api/auth', require('./server/api/auth'));
