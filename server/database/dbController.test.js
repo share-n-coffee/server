cfe33b08b1bcad7596eaa9f7da4c4571b5aa8d93
DasneiN <sank.mail@gmail.com>
2019-05-11 20:19:09 +0300

    add: tests for dbController -> {getUserByTelegramId, getEventById}

--- server/database/dbController.test.js
+++ server/database/dbController.test.js
@@ -5,43 +5,74 @@ const demoEvents = require('./models/demo/event.js');
 const demoDepartments = require('./models/demo/department.js');
 const controller = require('./dbController.js');
 
+const testData = {
+  userTelegramId: undefined,
+  eventId: undefined
+};
+
 connectDatabase();
 
-describe('dbcontroller tests', () => {
+describe('dbController tests', () => {
   it('config test', () => {
     expect(controller).toBeTruthy();
   });
 
   test('Get all Events', done => {
-    function callback(data) {
-      expect(data.length).toBe(5);
+    function cb(data) {
+      // eslint-disable-next-line dot-notation
+      testData.eventId = data[0]['_id'];
+      expect(data).toHaveLength(5);
       done();
     }
 
     controller.getAllEvents().then(events => {
-      callback(events);
+      cb(events);
     });
   });
 
   test('Get all Users', done => {
-    function callback(data) {
-      expect(data.length).toBe(20);
+    function cb(data) {
+      testData.userTelegramId = data[0].telegramUserId;
+
+      expect(data).toHaveLength(20);
       done();
     }
 
     controller.getAllUsers().then(users => {
-      callback(users);
+      cb(users);
     });
   });
 
   test('Get all Departments', done => {
-    function callback(data) {
-      expect(data.length).toBe(5);
+    function cb(data) {
+      expect(data).toHaveLength(5);
       done();
     }
 
     controller.getAllDepartments().then(department => {
-      callback(department);
+      cb(department);
+    });
+  });
+
+  test('GET User by telegram id', done => {
+    function cb(user) {
+      expect(user).toHaveProperty('username');
+      done();
+    }
+
+    controller.getUserByTelegramId(testData.userTelegramId).then(user => {
+      cb(user);
+    });
+  });
+
+  test('GET Event by _id', done => {
+    function cb(event) {
+      expect(event).toHaveProperty('description');
+      done();
+    }
+
+    controller.getEventById(testData.eventId).then(event => {
+      cb(event);
     });
   });
 });
