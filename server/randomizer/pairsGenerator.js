f168514e2b7cd426390ce91d593eae3637fc0483
filipovichjenya <a.hz@list.ru>
2019-05-11 21:24:04 +0300

    fix

--- server/randomizer/pairsGenerator.js
+++ server/randomizer/pairsGenerator.js
@@ -1,6 +1,24 @@
 function pairsGenerator(allData) {
-  // -- Ваш код -- //
-  console.log(allData);
+  const eventsData = allData[0];
+  const departmentsData = allData[1];
+  const usersData = allData[2];
+
+  const events = {};
+  eventsData.forEach(event => {
+    events[event.id] = {};
+  });
+
+  usersData.forEach(user => {
+    user.events.forEach(userEvent => {
+      if (events[userEvent][user.department]) {
+        events[userEvent][user.department].push(user.telegram_user_id);
+      } else {
+        events[userEvent][user.department] = [user.telegram_user_id];
+      }
+    });
+  });
+
+  console.log(events);
 }
 
 module.exports = pairsGenerator;
