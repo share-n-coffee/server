ff3aac91bb39f3255349950d17e46baa1b3ef01c
Dmitry <jimmaggot@mail.ru>
2019-05-16 02:26:35 +0300

    fix: change DBController signature, add randomizerMetodsFactory

--- server/database/dbController.js
+++ server/database/dbController.js
@@ -1,27 +1,45 @@
 const userMethodsFactory = require('./factories/userMethodsFactory');
 const eventMethodsFactory = require('./factories/eventMethodsFactory');
 const departmentMethodsFactory = require('./factories/departmentMethodsFactory');
+const randomizerMethodsFactory = require('./factories/randomizerMethodsFactory');
+const collection = require('./collection');
 
-function DBController(user, event, department) {
-  let userModel;
-  let eventModel;
-  let departmentModel;
+function DBController(collectionName) {
   if (arguments.length === 0) {
-    userModel = 'demo_user';
-    eventModel = 'demo_event';
-    departmentModel = 'demo_department';
-  } else {
-    userModel = user;
-    eventModel = event;
-    departmentModel = department;
+    return Object.assign(
+      userMethodsFactory(collection.users),
+      eventMethodsFactory(collection.events),
+      departmentMethodsFactory(collection.departments),
+      randomizerMethodsFactory({
+        eventPairs: collection.eventPairs,
+        eventReserve: collection.eventReserve
+      })
+    );
   }
-  console.log('user, event, dep ', userModel, eventModel, departmentModel);
-  Object.assign(
-    this,
-    userMethodsFactory(userModel),
-    eventMethodsFactory(eventModel),
-    departmentMethodsFactory(departmentModel)
-  );
+  if (arguments.length === 1) {
+    if (typeof collectionName !== 'string') {
+      throw new TypeError('DBController argument should be a strind');
+    }
+    switch (collectionName) {
+      case 'user':
+        return userMethodsFactory(collection.users);
+      case 'event':
+        return eventMethodsFactory(collection.events);
+      case 'department':
+        return departmentMethodsFactory(collection.departments);
+      case 'randomizer':
+        return randomizerMethodsFactory({
+          eventPairs: collection.eventPairs,
+          eventReserve: collection.eventReserve
+        });
+      default:
+        throw new TypeError('Argument should be a collection name');
+    }
+  } else {
+    throw new SyntaxError(
+      'DBController constructor should have one or none arguments'
+    );
+  }
 }
 
 module.exports = DBController;
