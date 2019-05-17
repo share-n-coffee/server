a7405d6542fff3bb5aec2adf1498b2ca3e6d507b
Crypto-kittensSeller <Nikolay.Shaveiko@gmail.com>
2019-05-17 11:43:57 +0300

    feat: make randController as a class

--- server/randomizer/randController.js
+++ server/randomizer/randController.js
@@ -3,23 +3,45 @@ const DBController = require('../database/dbController');
 // eslint-disable-next-line import/order
 // eslint-disable-next-line prefer-destructuring
 const CronJob = require('cron').CronJob;
-const randomizer = require('./pairsGenerator');
+const pairsGenerator = require('./pairsGenerator');
 const checkData = require('./checkDataCorrectness');
 
 const controller = new DBController();
 
-const job = new CronJob('*/5 * * * * *', () => {
-  Promise.all([
-    controller.getAllEvents(),
-    controller.getAllDepartments(),
-    controller.getAllUsers()
-  ]).then(allData => {
-    checkData(allData);
-    /* controller.removeEventPairs().then(() => {
-      pairsGenerator(data); // <===  Функция генерации пар//
-    }); */
-  });
-});
+class RandController {
+  static checkAllData() {
+    Promise.all([
+      controller.getAllEvents(),
+      controller.getAllDepartments(),
+      controller.getAllUsers()
+    ])
+      .then(allData => {
+        checkData(allData);
+      })
+      .catch(error => {
+        throw Error(error);
+      });
+  }
 
-module.exports = job.start();
+  static async selectEventForPairGenerating(eventId) {
+    const eventToBalance = await controller.getEventById(eventId);
+    const allUsers = await controller.getAllUsers();
+
+    if (eventToBalance.active) {
+      pairsGenerator(eventToBalance, allUsers);
+    } else {
+      throw Error('Event has been disabled');
+    }
+  }
+}
+
+/* const job = new CronJob('* * * * * *', () => {
+   controller.removeEventPairs().then(() => {
+      pairsGenerator(data); // <===  Функция генерации пар//
+    }); 
+});
+job.start();
+*/
+
+module.exports = RandController;
 // Тестовая попытка запуска рандомайзера //
