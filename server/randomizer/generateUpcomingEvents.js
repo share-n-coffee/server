444780cac262ea3f90981db00c2d1bb5579c1984
DasneiN <sank.mail@gmail.com>
2019-05-18 12:57:56 +0300

    feat: start writing function

---
+++ server/randomizer/generateUpcomingEvents.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const ClassDBController = require('../database/dbController');
+
+const app = express();
+const port = 9000;
+
+function generateUpcomingEvents(period = 'month') {
+  const periodDays =
+    period === 'month'
+      ? new Date(new Date().getFullYear(), new Date().getMonth(), 0).getDate()
+      : 7;
+
+  const endTime = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth(),
+    0
+  ).getTime();
+  const startTime = endTime - periodDays * 24 * 60 * 60;
+  console.log('generate');
+}
+
+// module.exports = generateUpcomingEvents;
+
+app.listen(port, err => {
+  if (err) throw err;
+  console.log(`> Ready on http://localhost:${port}`);
+
+  const DBController = new ClassDBController('event');
+  DBController.getAllEvents()
+    .then(events => console.log(events))
+    .catch(error => console.log(error));
+});
