6418058757fe4829acbdd58a4cc5629e14ddc355
DasneiN <sank.mail@gmail.com>
2019-05-18 22:48:10 +0300

    feat: implement api update department

--- server/api/apiHandles/apiDepartments.js
+++ server/api/apiHandles/apiDepartments.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const { ObjectId } = require('mongoose').Types;
 const ClassDBController = require('../../database/dbController');
-const jwtAuth = require('../../middleware/jwtAuth');
 const adminAuth = require('../../middleware/adminAuth');
+const objectIdValidation = require('./../../middleware/objectIdValidation');
 
 const router = express.Router();
 
@@ -29,18 +29,22 @@ router
     }
   });
 
-router.route('/:id').get((req, res) => {
-  const departmentId = req.params.id;
-
-  if (ObjectId.isValid(departmentId)) {
+router
+  .route('/:id', objectIdValidation)
+  .get((req, res) => {
     const DBController = new ClassDBController('department');
 
-    DBController.getDepartmentById(departmentId)
+    DBController.getDepartmentById(req.params.id)
       .then(department => res.status(200).json(department))
       .catch(error => res.status(404).send(error));
-  } else {
-    res.status(404).send('Request query must be a valid ObjectId!');
-  }
-});
+  })
+  .put((req, res) => {
+    const DBController = new ClassDBController('department');
+    DBController.updateDepartment(req.params.id, req.body)
+      .then(updatedDepartment => {
+        res.status(200).json(updatedDepartment);
+      })
+      .catch(error => res.status(404).send(error));
+  });
 
 module.exports = router;
