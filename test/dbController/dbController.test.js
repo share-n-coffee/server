aa61a23abc872097379be3efa3e2e1fd6c1f31e0
Dmitry <jimmaggot@mail.ru>
2019-05-19 14:05:43 +0300

    feat: move dbController tests to tests folder

--- test/dbController/dbController.test.js
+++ test/dbController/dbController.test.js
@@ -1,10 +1,8 @@
 /* eslint-disable dot-notation */
 const mongoose = require('mongoose');
-const { MongoClient } = require('mongodb');
-const config = require('./../config/config');
-const connectDatabase = require('../lib/connectDatabase');
-const ClassController = require('./dbController');
-const collectionConfig = require('./collection');
+const connectDatabase = require('../../server/lib/connectDatabase');
+const ClassController = require('../../server/database/dbController');
+// const collectionConfig = require('./collection');
 
 const userMethods = [
   'getAllUsers',
@@ -67,143 +65,6 @@ describe('dbController tests', () => {
       expect(testMethods.includes(method)).toBeTruthy();
     });
     done();
-  });
-
-  test('Get all Events', done => {
-    function cb(data) {
-      testData.eventId = data[0]['_id'];
-      expect(data).toHaveLength(5);
-      done();
-    }
-
-    controller.getAllEvents().then(events => {
-      cb(events);
-    });
-  });
-
-  test('Get all Users', done => {
-    function cb(controllerUsers, mongoUsersCount) {
-      testData.userTelegramId = controllerUsers[0].telegramUserId;
-      testData.userId = controllerUsers[0]['_id'];
-      expect(controllerUsers).toHaveLength(mongoUsersCount);
-      done();
-    }
-
-    //  Send mongodb driver's query for comparing results
-    MongoClient.connect(
-      config.database,
-      (err, client) => {
-        client
-          .db('demoproject')
-          .collection('demo_users')
-          .find({})
-          .count((error, mongoUsersCount) => {
-            client.close();
-            controller.getAllUsers().then(controllerUsers => {
-              cb(controllerUsers, mongoUsersCount);
-            });
-          });
-      }
-    );
-  });
-
-  test('Get all Departments', done => {
-    function cb(controllerDepartments, mongoDepartmentsCount) {
-      testData.departmentId = controllerDepartments[0]['_id'];
-
-      expect(controllerDepartments).toHaveLength(mongoDepartmentsCount);
-      done();
-    }
-    MongoClient.connect(
-      config.database,
-      (err, client) => {
-        client
-          .db('demoproject')
-          .collection('demo_departments')
-          .find({})
-          .count((error, mongoDepartmentsCount) => {
-            client.close();
-            controller.getAllDepartments().then(controllerDepartments => {
-              cb(controllerDepartments, mongoDepartmentsCount);
-            });
-          });
-      }
-    );
-  });
-
-  // test('GET User by telegram id', done => {
-  //   function cb(user) {
-  //     expect(user).toHaveProperty('username');
-  //     done();
-  //   }
-
-  //   controller.getUserById(testData.userTelegramId).then(user => {
-  //     cb(user);
-  //   });
-  // });
-
-  test('GET User by _id', done => {
-    function cb(user) {
-      expect(user).toHaveProperty('username');
-      done();
-    }
-
-    controller.getUserById(testData.userId).then(user => {
-      cb(user);
-    });
-  });
-
-  test('GET Event by _id', done => {
-    function cb(event) {
-      expect(event).toHaveProperty('description');
-      done();
-    }
-
-    controller.getEventById(testData.eventId).then(event => {
-      cb(event);
-    });
-  });
-
-  test('GET Department by _id', done => {
-    function cb(department) {
-      expect(department).toHaveProperty('description');
-      done();
-    }
-
-    controller.getDepartmentById(testData.departmentId).then(department => {
-      cb(department);
-    });
-  });
-
-  test('POST new Department', done => {
-    const newDepartmentParams = {
-      title: 'New Department',
-      description: 'Just another Department'
-    };
-
-    function cb(newDepartment) {
-      expect(newDepartment.description).toMatch(
-        newDepartmentParams.description
-      );
-
-      //  Send mongodb driver's query for deleting test Department
-      MongoClient.connect(
-        config.database,
-        (err, client) => {
-          client
-            .db('demoproject')
-            .collection('demo_departments')
-            .deleteOne({ _id: newDepartment['_id'] }, () => {
-              client.close();
-              done();
-            });
-        }
-      );
-    }
-
-    controller.postNewDepartment(newDepartmentParams).then(newDepartment => {
-      cb(newDepartment);
-    });
   });
 });
 
