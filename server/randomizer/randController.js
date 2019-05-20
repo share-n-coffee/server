30209f1b83aa85bc2279bbed8c297c28cd4b49d1
Crypto-kittensSeller <Nikolay.Shaveiko@gmail.com>
2019-05-20 12:13:32 +0300

    refactor: change methods names

--- server/randomizer/randController.js
+++ server/randomizer/randController.js
@@ -8,7 +8,7 @@ const checkData = require('./checkDataCorrectness');
 const addNewPairs = require('./substitution');
 
 const controller = new DBController();
-const substitutionQueue = [];
+const substitutionQueue = ['5cd6f6c381371d297acb2fe0'];
 const incommingCyclicEventsStorage = [];
 
 const substitution = new CronJob('*/30 * * * * *', () => {
@@ -35,13 +35,15 @@ class RandController {
   }
 
   static async selectEventForPairGenerating(eventId) {
-    const eventToBalance = await controller.getEventById(eventId);
-    const allUsers = await controller.getAllUsers();
+    const eventToBalance = await controller
+      .getEventById(eventId)
+      .catch(error => {
+        console.log(error);
+      });
 
     if (eventToBalance.active) {
-      controller.removeEventPairs().then(() => {
-        pairsGenerator(eventToBalance, allUsers); // <===  Функция генерации пар//
-      });
+      await controller.removeEventPairByEventId(eventId);
+      pairsGenerator(eventToBalance); // <===  Функция генерации пар//
     } else {
       throw Error('Event has been disabled');
     }
