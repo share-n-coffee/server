937ea6e7c9156888570ec4b6ebcbd7f29187953c
Dmitry <jimmaggot@mail.ru>
2019-05-21 16:19:55 +0300

    feat: refactor db scheme for event

--- server/database/models/event.js
+++ server/database/models/event.js
@@ -3,62 +3,24 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const EventSchema = new Schema({
-  //  Состояние события.
-  //  Если админ скрыл событие или одиночное событие прошло, то переводим флаг в false
-  active: {
-    type: Boolean,
-    required: true,
-    default: false
-  },
-  // Название события
-  title: {
-    type: String,
+  //  id топика
+  topicId: {
+    type: mongoose.Schema.Types.ObjectId,
     required: true
   },
-  //  Описание события
-  description: {
-    type: String,
-    required: true
-  },
-  //  Координаты места проведения
-  //  Формат: [7.1854773, 1.9979411]
-  location: {
-    type: Array,
-    required: true
-  },
-  //  Адррес места проведения
-  address: {
-    type: String,
-    required: false
-  },
-  options: {
-    // Является ли событие повторяющимся
-    cyclic: {
-      type: Boolean,
-      required: true,
-      default: false
-    },
-    //  День недели, начиная с воскресенья, в который проводится событие
-    //  Для одиночного события – не имеет значения
-    regularity: {
-      type: Number,
-      required: false
-    },
-    //  Даты следующих событий, пересчитывается отдельным скриптом перед работой рандомайзера
-    //  Если создаётся одиночное событие, то этот параметр ему выставляется сразу, один элементом массива
-    nextDates: [
-      {
-        type: Number,
-        required: false
-      }
-    ]
-  },
-  //  Дата добавления события в БД
-  created: {
-    type: Number,
-    default: new Date().getTime(),
-    required: false
-  }
+  // название встречи ( ==  topic.title )
+  eventTitle: { type: String, required: false },
+  //  список участников и их статусов
+  participants: [
+    {
+      userId: { type: mongoose.Schema.Types.ObjectId, required: false },
+      status: { type: String, required: false, default: 'free' }
+    }
+  ],
+  // место проведения ( == topic.location )
+  place: { type: String, required: false },
+  // дата проведения
+  date: { type: Number, required: false }
 });
 
 module.exports = modelName => mongoose.model(modelName, EventSchema);
