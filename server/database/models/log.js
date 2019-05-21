14c2aaada430e6c8898b79af1a4b3bc40cc9b734
saltanovich <i_am_good@tut.by>
2019-05-21 06:27:15 +0300

    log model

---
+++ server/database/models/log.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const { Schema, model } = mongoose;
+
+const LogSchema = new Schema({
+  userId: {
+    type: Number,
+    required: true
+  },
+  actionType: {
+    type: String,
+    required: true
+  },
+  action: {
+    type: String,
+    required: true
+  },
+  timestamp: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = modelName => model(modelName, LogSchema);
