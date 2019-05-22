0d9669da27c7e7d12ebe5868d91f8b14efab6c12
Crypto-kittensSeller <Nikolay.Shaveiko@gmail.com>
2019-05-22 16:51:44 +0300

    feat: add cron for events creation

--- server/randomizer/randController.js
+++ server/randomizer/randController.js
@@ -3,21 +3,12 @@ const DBController = require('../database/dbController');
 // eslint-disable-next-line import/order
 // eslint-disable-next-line prefer-destructuring
 const CronJob = require('cron').CronJob;
-const pairsGenerator = require('./pairsGenerator');
 const checkData = require('./checkDataCorrectness');
 const addNewPairs = require('./substitution');
+const generateUpcomingDatesByTopic = require('./generateUpcomingDatesByTopic');
 
 const controller = new DBController();
 const substitutionQueue = [];
-const incommingCyclicEventsStorage = [];
-
-const substitution = new CronJob('*/30 * * * * *', () => {
-  if (substitutionQueue.length !== 0) {
-    addNewPairs(substitutionQueue.pop());
-  }
-});
-
-substitution.start();
 
 class RandController {
   static checkAllData() {
@@ -34,19 +25,26 @@ class RandController {
       });
   }
 
-  static async selectEventForPairGenerating(eventId) {
-    const eventToBalance = await controller
-      .getEventById(eventId)
-      .catch(error => {
-        console.log(error);
-      });
+  static async generateEventsForTopics() {
+    const allTopics = await controller.getAllTopics();
+    allTopics.forEach(async topic => {
+      if (topic.active === false) return;
 
-    if (eventToBalance.active) {
-      await controller.removeAllEventPairsByEventId(eventId);
-      pairsGenerator(eventToBalance); // <===  Функция генерации пар//
-    } else {
-      throw Error('Event has been disabled');
-    }
+      if (topic.cyclic === true) {
+        const dates = generateUpcomingDatesByTopic(topic);
+        dates.forEach(date => {
+          RandController.createEvent(topic.id, date);
+        });
+        const dateOfEventsСreation = new Date();
+        await controller.updateLastEventsCreatingDate(); // сделать метод, если нет еще
+      } else {
+        RandController.createEvent(topic.id, topic.date);
+      }
+    });
+  }
+
+  static async createEvent(topicId, date) {
+    // создает событие как объект новой коллекции и добавляет в базу
   }
 
   static makeSubstitution(eventId) {
@@ -57,3 +55,37 @@ class RandController {
 // RandController.makeSubstitution('5cd6f6c381371d297acb2fe0'); // метод для вызова ботом в случае отказа пользователя
 
 module.exports = RandController;
+
+const substitution = new CronJob('*/30 * * * * *', () => {
+  if (substitutionQueue.length !== 0) {
+    addNewPairs(substitutionQueue.pop());
+  }
+});
+
+substitution.start();
+
+const eventsGenerator = new CronJob('*/30 * * * * *', () => {
+  RandController.generateEventsForTopics();
+});
+
+eventsGenerator.start();
+
+// тестируем работу генерации дат
+/*
+const dates = generateUpcomingDatesByTopic({
+  _id: '5cd6f6c381371d297acb2fe0',
+  active: true,
+  title: 'Furnitech',
+  description:
+    'Irure tempor qui tempor id mollit aliquip pariatur est nisi. Exercitation consequat eiusmod non Lorem nisi. Pariatur aute excepteur laborum cupidatat sint fugiat. Reprehenderit do laborum Lorem tempor dolore. Dolore eiusmod qui duis enim ut ex.',
+  location: ['53.886666', '27.53039'],
+  address: '594 Neptune Court, Rehrersburg, Tennessee',
+  cyclic: true,
+  weekDay: 3,
+  time: '11:45',
+  created: 1558161349720,
+  lastEventsCreationDate: 1560624944000
+});
+
+console.log(dates);
+*/
