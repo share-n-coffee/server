19f3f4262be087edb596d6c250c71911d0853a1d
Crypto-kittensSeller <Nikolay.Shaveiko@gmail.com>
2019-05-22 16:04:56 +0300

    feat: add cyclic dates generation by topic

--- server/randomizer/generateUpcomingDatesByTopic.js
+++ server/randomizer/generateUpcomingDatesByTopic.js
@@ -1,14 +1,28 @@
-/* eslint-disable prettier/prettier */
-// Функция принимает объект event и генерирует события на следущий месяц и заносит их в event.options.nexDates
-function generateUpcomingDatesByTopic({ options, created }) {
-  // 00:00:00 первого числа следующего месяца
-  const firstDayNextMonth = new Date(
-    new Date(created).getFullYear(),
-    new Date(created).getMonth() + 1,
-    1,
-    3
-  );
-  // 23:59:59 последнего числа месяца
+// takes event returns dates for next month since last generating
+function generateUpcomingDatesByTopic({
+  weekDay,
+  time,
+  created,
+  lastEventsCreationDate
+}) {
+  const nextDates = [];
+  let firstDayNextMonth = new Date();
+  if (lastEventsCreationDate === undefined) {
+    firstDayNextMonth = new Date(
+      new Date(created).getFullYear(),
+      new Date(created).getMonth() + 1,
+      1,
+      3
+    );
+  } else {
+    firstDayNextMonth = new Date(
+      new Date(lastEventsCreationDate).getFullYear(),
+      new Date(lastEventsCreationDate).getMonth() + 1,
+      1,
+      3
+    );
+  }
+
   const endOfMonth = new Date(
     firstDayNextMonth.getFullYear(),
     firstDayNextMonth.getMonth() + 1,
@@ -17,29 +31,28 @@ function generateUpcomingDatesByTopic({ options, created }) {
     59,
     59
   );
+
   const oneDay = 60 * 60 * 24 * 1000;
 
   const findDates = timestamp => {
     let currentTime = timestamp;
+    const eventTime = time;
     if (currentTime > endOfMonth) {
-      return options.nextDates;
+      return nextDates;
     }
     const currentDay = new Date(currentTime).getDay();
-    if (currentDay === options.regularity) {
+    if (currentDay === weekDay) {
       const year = new Date(currentTime).getFullYear();
       const month = new Date(currentTime).getMonth();
       const day = new Date(currentTime).getDate();
-      const hour = options.hour + 3;
-      options.nextDates.push(new Date(year, month, day, hour));
-      if (!options.cyclic) {
-        return options.nextDates;
-      }
+      const hour = +eventTime.slice(0, eventTime.indexOf(':')) + 2;
+      const minutes = eventTime.slice(eventTime.indexOf(':') + 1);
+      nextDates.push(new Date(year, month, day, hour, minutes));
     }
     findDates((currentTime += oneDay));
-    return options.nextDates;
+    return nextDates;
   };
-
-  findDates(firstDayNextMonth.getTime());
+  return findDates(firstDayNextMonth.getTime());
 }
 
 module.exports = generateUpcomingDatesByTopic;
