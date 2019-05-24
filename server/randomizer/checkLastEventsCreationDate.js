85443c2eb83e78cb1be0ab257a1f5569860a3e04
Crypto-kittensSeller <Nikolay.Shaveiko@gmail.com>
2019-05-24 12:05:05 +0300

    feat: implement last events creation checking

---
+++ server/randomizer/checkLastEventsCreationDate.js
@@ -0,0 +1,28 @@
+function checkLastEventsCreationDate(lastDate) {
+  const currentDate = new Date();
+
+  const firstDayPreviousMonth = new Date(
+    new Date(currentDate).getFullYear(),
+    new Date(currentDate).getMonth() - 1,
+    1,
+    2
+  );
+
+  const lastDayPreviousMonth = new Date(
+    firstDayPreviousMonth.getFullYear(),
+    firstDayPreviousMonth.getMonth() + 1,
+    1,
+    1,
+    59,
+    59
+  );
+  console.log(firstDayPreviousMonth);
+  console.log(lastDayPreviousMonth);
+
+  if (lastDate > firstDayPreviousMonth && lastDate < lastDayPreviousMonth) {
+    return true;
+  }
+  return false;
+}
+
+module.exports = checkLastEventsCreationDate;
