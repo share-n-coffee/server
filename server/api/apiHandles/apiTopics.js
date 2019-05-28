01e92873b6725a0b522f00767d4160915ffc971a
DasneiN <sank.mail@gmail.com>
2019-05-29 01:42:14 +0300

    feat: add /events/:topicId with users full information

--- server/api/apiHandles/apiTopics.js
+++ server/api/apiHandles/apiTopics.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 const express = require('express');
 const { ObjectId } = require('mongoose').Types;
 const ClassDBController = require('../../database/dbController');
@@ -97,5 +98,67 @@ router
       res.status(404).send('TopicId & userId is not valid ObjectId!');
     }
   });
+
+router.route('/events/:topicId').get((req, res) => {
+  if (ObjectId.isValid(req.params.topicId)) {
+    const DBController = new ClassDBController('user', 'event');
+
+    req.query = {
+      topicId: req.params.topicId
+    };
+
+    DBController.findEvents(req)
+      .then(allEvents => {
+        const resEvents = Array.from(allEvents);
+        const users = [];
+
+        resEvents.forEach(event => {
+          event.participants.forEach(participant => {
+            if (!users.includes(participant.userId)) {
+              users.push(participant.userId);
+            }
+          });
+        });
+
+        const usersPromises = users.map(
+          userId =>
+            new Promise((resolve, reject) => {
+              DBController.findOneUser(
+                { _id: userId },
+                'firstName lastName'
+              ).then(user =>
+                resolve({
+                  [userId]: user
+                })
+              );
+            })
+        );
+
+        Promise.all(usersPromises)
+          .then(usersData => {
+            let usersInfo = {};
+            for (let i = 0; i < usersData.length; i++) {
+              usersInfo = {
+                ...usersInfo,
+                ...usersData[i]
+              };
+            }
+
+            for (let i = 0; i < resEvents.length; i++) {
+              for (let j = 0; j < resEvents[i].participants.length; j++) {
+                resEvents[i].participants[j] =
+                  usersInfo[resEvents[i].participants[j].userId];
+              }
+            }
+
+            return res.status(200).json({ data: resEvents });
+          })
+          .catch(error => res.status(404).send(error));
+      })
+      .catch(error => res.status(404).send(error));
+  } else {
+    res.status(404).send('TopicId is not valid ObjectId!');
+  }
+});
 
 module.exports = router;
