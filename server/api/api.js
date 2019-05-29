f49811d20d25594244391c32c8206bfc04bb3d66
DasneiN <sank.mail@gmail.com>
2019-05-29 18:41:30 +0300

    remove: api route for randomizer

--- server/api/api.js
+++ server/api/api.js
@@ -3,7 +3,6 @@ const apiTopics = require('./apiHandles/apiTopics');
 const apiEvents = require('./apiHandles/apiEvents');
 const apiUsers = require('./apiHandles/apiUsers');
 const apiDepartments = require('./apiHandles/apiDepartments');
-const apiRandomizer = require('./apiHandles/apiRandomizer');
 const apiSubscriptions = require('./apiHandles/apiSubscriptions');
 
 const router = express.Router();
@@ -12,7 +11,6 @@ router.use('/users/', apiUsers);
 router.use('/topics/', apiTopics);
 router.use('/events/', apiEvents);
 router.use('/departments/', apiDepartments);
-router.use('/randomizer/', apiRandomizer);
 router.use('/subscriptions/', apiSubscriptions);
 
 module.exports = router;
